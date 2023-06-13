f780acc1898b26f1b2a183679b2057d61a4efd3d
André Souza <132021448+eiandresouza@users.noreply.github.com>
2023-06-13 14:40:11 -0300

    Update client.js

--- Recurso/FivemVehicle/client.js
+++ Recurso/FivemVehicle/client.js
@@ -1,3 +1,5 @@
+// client.js
+
 // Registra o comando para gerar um veículo
 RegisterCommand('car', async (source, args) => {
     try {
@@ -17,37 +19,41 @@ RegisterCommand('car', async (source, args) => {
         const currentVehicle = GetVehiclePedIsIn(playerPed, false);
 
         if (currentVehicle !== 0) {
+            // Obtém as coordenadas e a rotação do veículo atual
+            const [x, y, z] = GetEntityCoords(currentVehicle);
+            const heading = GetEntityHeading(currentVehicle);
+
             // Se o jogador estiver em um veículo, deleta o veículo atual
             DeleteEntity(currentVehicle);
-        }
 
-        // Obtém as coordenadas do jogador
-        const [x, y, z] = GetEntityCoords(playerPed);
+            // Gera o novo veículo na mesma posição e rotação
+            const vehicle = CreateVehicle(modelHash, x, y, z, heading, false, false);
 
-        // Gera o veículo
-        const vehicle = CreateVehicle(modelHash, x, y, z, 0, false, false);
+            // Obtém o ID de rede do veículo
+            const networkId = NetworkGetNetworkIdFromEntity(vehicle);
 
-        // Obtém o ID de rede do veículo
-        const networkId = NetworkGetNetworkIdFromEntity(vehicle);
+            if (networkId !== 0) {
+                // Marca o veículo como não mais necessário
+                SetVehicleAsNoLongerNeeded(vehicle);
 
-        if (networkId !== 0) {
-            // Marca o veículo como não mais necessário
-            SetVehicleAsNoLongerNeeded(vehicle);
+                // Marca o modelo do veículo como não mais necessário
+                SetModelAsNoLongerNeeded(vehicleModel);
 
-            // Marca o modelo do veículo como não mais necessário
-            SetModelAsNoLongerNeeded(vehicleModel);
+                // Coloca o jogador dentro do veículo
+                SetPedIntoVehicle(playerPed, vehicle, -1);
 
-            // Coloca o jogador dentro do veículo
-            SetPedIntoVehicle(playerPed, vehicle, -1);
+                // Registra uma mensagem de sucesso
+                console.log('Veículo gerado:', vehicleModel);
+            } else {
+                // Registra uma mensagem de erro se a geração falhar
+                console.error('Falha ao gerar veículo:', vehicleModel);
 
-            // Registra uma mensagem de sucesso
-            console.log('Veículo gerado:', vehicleModel);
+                // Exclui a entidade do veículo se o ID de rede for inválido
+                DeleteEntity(vehicle);
+            }
         } else {
-            // Registra uma mensagem de erro se a geração falhar
-            console.error('Falha ao gerar veículo:', vehicleModel);
-
-            // Exclui a entidade do veículo se o ID de rede for inválido
-            DeleteEntity(vehicle);
+            // Registra uma mensagem de erro se o jogador não estiver em um veículo
+            console.error('Erro: Você não está em um veículo.');
         }
     } catch (error) {
         // Registra uma mensagem de erro se a geração falhar
